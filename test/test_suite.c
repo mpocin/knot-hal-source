f7da1f6fdf53d9fbc677034eeb3cf9e68135f865
Marcela Oliveira <mpo@cin.ufpe.br>
2017-02-07 18:38:31 -0300

    add skeleton of test group

--- test/test_suite.c
+++ test/test_suite.c
@@ -8,13 +8,41 @@
  */
 
 #include <CppUTest/TestHarness.h>
-#include "all_tests"
+#include "all_tests.c"
 
+extern "C"
+{
+#include "sample.h" // tested files
+}
+
+//START: testGroup
 TEST_GROUP(FirstTestGroup)
 {
+        // initial setup
+        void setup()
+        {
+                memset(output, 0xaa, sizeof output);
+                expected = "";
+        }
+        
+        // cleanup
+        void teardown()
+        {
+                
+        }
+        
+        // helper functions accessible to all test() cases in testGroup
 };
+//END: testGroup
 
 TEST(FirstTestGroup, FirstTest)
 {
    FAIL("Fail me!");
+}
+
+TEST(FirstTestGroup, SecondTest)
+{
+   STRCMP_EQUAL("hello", "world");
+   LONGS_EQUAL(1, 2);
+   CHECK(false);
 }
