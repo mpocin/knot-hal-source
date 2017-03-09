1401dc28a98a075b42ccb45dc33c8706c09decc4
Marcela Oliveira <mpo@cin.ufpe.br>
2017-03-09 11:13:06 -0300

    Unify KNoT Thing user configuration
    Provide a single file for THING's configuration. User can change
     the name of thing and amount of data (sensors/actuators) to be
     used

--- include/config.h
+++ include/config.h
@@ -1,7 +1,30 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ * Configuration file for KNOT THING
+ */
+
 #ifndef __HAL_CONFIG_H__
 #define __HAL_CONFIG_H__
 
-#define THING_NAME "thingname"
+/*
+ * User may modify the amount of data (sensors/actuators) to be connected
+ * to THING
+ * Default value = 5
+ * Values allowed are: MIN = 1 and MAX = 5
+ */
+#define KNOT_THING_NUM_DATA             5
+
+/*
+ * User may modify the name of thing
+ * Default value = "thingname"
+ * Empty value is not allowed
+ */
+#define THING_NAME                      "thingname"
 
 
 #endif /* __HAL_CONFIG_H__ */
